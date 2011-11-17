936d04b60a5516cb81b028c3a9730a65589e3f77
Miles McCrocklin - work MBP <miles.mccrocklin@gmail.com>
2011-11-17 12:53:50 -0800

    Ideas Documented

---
+++ install.py
@@ -0,0 +1,18 @@
+#figure out which system your on 
+#(e.g OS X | FreeBSD | Linux (should this be granular 
+#based off version/distro?)
+operating_system = ''
+
+#figure out what language is available to you & or install
+#this script up to heremay be converted to C and allow you to update 
+#or install python and other languages
+
+#install package managers necessary if not installed
+pkg-mgr = []
+#e.g /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+
+packages = []
+#install every package keeping in mind several things:
+#dependencies
+#over-loading commands 
+
